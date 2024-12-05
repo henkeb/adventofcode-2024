aaedb83d76abeddd7b28e7e5a88db5100d0cdc31
Henrik Berg <khp.berg@gmail.com>
2024-12-05 10:15:43 +0100

    Minor update

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -1,9 +1,9 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 
-fn handle_input(input: &str) -> (HashMap<usize, Vec<usize>>, &str) {
+fn handle_input(input: &str) -> (HashMap<usize, Vec<usize>>, Vec<Vec<usize>>) {
     let mut parts = input.split("\n\n");
     let rules = parts.next().unwrap();
-    let updates = parts.next().unwrap();
+    let updates_raw = parts.next().unwrap();
 
     let mut rules_map_number_before: HashMap<usize, Vec<usize>> = HashMap::new();
 
@@ -18,6 +18,17 @@ fn handle_input(input: &str) -> (HashMap<usize, Vec<usize>>, &str) {
                 .or_insert(vec![after]);
         });
 
+    let mut updates: Vec<Vec<usize>> = Vec::new();
+
+    for raw_update in updates_raw.lines() {
+        updates.push(
+            raw_update
+                .split(',')
+                .filter_map(|val| val.parse::<usize>().ok())
+                .collect(),
+        );
+    }
+
     (rules_map_number_before, updates)
 }
 
@@ -28,14 +39,10 @@ pub fn puzzle_1(input: &str) -> String {
     let (rules_map_number_before, updates) = handle_input(input);
 
     let mut count = 0;
-    for update in updates.lines() {
+    for update in updates.iter() {
         let mut handled_values: HashSet<usize> = HashSet::new();
-        let values: Vec<usize> = update
-            .split(',')
-            .filter_map(|val| val.parse::<usize>().ok())
-            .collect();
         let mut is_update_ok = true;
-        for value in values.iter() {
+        for value in update.iter() {
             if let Some(before_list) = rules_map_number_before.get(&value) {
                 for before in before_list.iter() {
                     if handled_values.contains(&before) {
@@ -50,8 +57,8 @@ pub fn puzzle_1(input: &str) -> String {
             handled_values.insert(*value);
         }
         if is_update_ok {
-            let len = &values.len() / 2;
-            count += values[len];
+            let len = &update.len() / 2;
+            count += update[len];
         }
     }
     count.to_string()
@@ -66,18 +73,14 @@ pub fn puzzle_2(input: &str) -> String {
     let mut failed_updates: Vec<Vec<usize>> = Vec::new();
 
     let mut count = 0;
-    for update in updates.lines() {
+    for update in updates.iter() {
         let mut handled_values: HashSet<usize> = HashSet::new();
-        let values: Vec<usize> = update
-            .split(',')
-            .filter_map(|val| val.parse::<usize>().ok())
-            .collect();
         let mut is_update_ok = true;
-        for value in values.iter() {
+        for value in update.iter() {
             if let Some(before_list) = rules_map_number_before.get(&value) {
                 for before in before_list.iter() {
                     if handled_values.contains(&before) {
-                        failed_updates.push(values.clone());
+                        failed_updates.push(update.clone());
                         is_update_ok = false;
                         break;
                     }
@@ -90,15 +93,15 @@ pub fn puzzle_2(input: &str) -> String {
         }
     }
 
-    for update in failed_updates.iter() {
+    for failed_update in failed_updates.iter() {
         let mut ordered: VecDeque<usize> = VecDeque::new();
-        for _ in 0..update.len() {
-            for val in update.iter() {
+        for _ in 0..failed_update.len() {
+            for val in failed_update.iter() {
                 if !ordered.contains(val) {
                     if let Some(before_list) = rules_map_number_before.get(val) {
                         let mut needs_before: HashSet<usize> = HashSet::new();
                         for before in before_list.iter() {
-                            if update.contains(before) {
+                            if failed_update.contains(before) {
                                 needs_before.insert(*before);
                             }
                         }
