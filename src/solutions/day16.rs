4b1401cfb85547ffb120930a653be116d9e194b3
Henrik Berg <khp.berg@gmail.com>
2024-12-16 09:43:30 +0100

    Solved part2, not an optimal solution. Needs to handle the case when we enter a tile on the optimal path that merges with another that needs to turn!

--- src/solutions/day16.rs
+++ src/solutions/day16.rs
@@ -42,7 +42,7 @@ fn bfs_with_cost(map: &mut Vec<Vec<char>>, starting_position: (isize, isize)) ->
             continue;
         }
         if let Some(prev_cost) = visited.get(&position) {
-            if cost > *prev_cost {
+            if cost > *prev_cost + 1000 {
                 continue;
             }
         }
@@ -81,19 +81,10 @@ fn bfs_with_cost(map: &mut Vec<Vec<char>>, starting_position: (isize, isize)) ->
             cost + 1000 + 1,
             path_rotation_2,
         ));
-        if position == (3, 9) {
-            let mut copy_map = map.clone();
-            for tile in path.iter() {
-                copy_map[tile.1 as usize][tile.0 as usize] = 'O';
-            }
-            println!("Cost: {cost}");
-            display_map(&copy_map);
-        }
     }
-    // for tile in best_path.iter() {
-    //     map[tile.1 as usize][tile.0 as usize] = 'O';
-    // }
-    // display_map(map);
+    for tile in best_path.iter() {
+        map[tile.1 as usize][tile.0 as usize] = 'O';
+    }
     (output, best_path.len())
 }
 
@@ -176,14 +167,39 @@ mod tests {
 #.###.#.#.#.#.#
 #S..#.....#...#
 ###############";
+    const INPUT2: &str = "#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################";
 
     #[test]
-    fn test_1() {
+    fn test_1a() {
         assert_eq!(puzzle_1(&INPUT), "7036");
+    }
+    #[test]
+    fn test_1b() {
+        assert_eq!(puzzle_1(&INPUT2), "11048");
     }
 
     #[test]
-    fn test_2() {
+    fn test_2a() {
         assert_eq!(puzzle_2(&INPUT), "45");
+    }
+    #[test]
+    fn test_2b() {
+        assert_eq!(puzzle_2(&INPUT2), "64");
     }
 }
